d52b16e5c2fd0c84d3d46c7ee7797d92468ca578
capedrazab <capedrazab@unal.edu.co>
2025-09-11 08:36:13 -0500

    commit

--- github_push.sh
+++ github_push.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 # crear claves pub ssh en local. Copiar a github -> settings -> ssh gpg
 #git remote set-url origin git@github.com:capedrazab/capedrazab.github.io.git
+git add -all
 git commit -m "first commit"
-git branch -M main 
+#git branch -M main 
 git push -u origin main
-git add --all
+
